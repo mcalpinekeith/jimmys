1fa441edc4fc7e0f752f2e25891a402071e7ec85
Keith McAlpine <mcalpinekeith@yahoo.com>
2023-12-12 12:32:20 -0600

    Workout Edit - ChangeNotifierProvider

--- lib/ui/screens/workout_edit/workout_edit_view_model.dart
+++ lib/ui/screens/workout_edit/workout_edit_view_model.dart
@@ -1,12 +1,9 @@
-import 'package:english_words/english_words.dart';
-import 'package:jimmys/domain/models/workout.dart';
 import 'package:jimmys/domain/models/workout_exercise.dart';
 import 'package:jimmys/domain/use_cases/exercise_use_cases.dart';
 import 'package:jimmys/domain/use_cases/workout_exercise_use_cases.dart';
 import 'package:jimmys/domain/use_cases/workout_use_cases.dart';
 import 'package:jimmys/ui/screens/_base/base_view_model.dart';
 import 'package:jimmys/ui/screens/workout_edit/workout_edit_contract.dart';
-import 'package:uuid/uuid.dart';
 
 class WorkoutEditViewModel extends BaseViewModel<WorkoutEditViewModelState, WorkoutEditVContract> implements WorkoutEditVMContract {
   final WorkoutUseCases _workoutInteractor;
@@ -24,56 +21,40 @@ class WorkoutEditViewModel extends BaseViewModel<WorkoutEditViewModelState, Work
 
   @override
   Future<void> onInitState() async {
-    vmState.isLoading = true;
-
-    await _loadWorkoutExercises();
-    await _loadCategories();
-    await _loadExercises();
-
-    stopLoadingState();
-  }
-
-  Future _loadWorkoutExercises() async {
     vmState.hasError = false;
+    startLoadingState();
 
     try {
-      vmState.workoutExercises.clear();
-
-      if (!vmState.isAdd) {
-        vmState.workoutExercises.addAll(await _workoutExerciseInteractor.get(criteria: ('workout_id', vmState.workout.id)));
-        vmState.workoutExercises.sort((a, b) => a.sequence.compareTo(b.sequence));
-      }
+      await _loadWorkoutExercises();
+      await _loadCategories();
+      await _loadExercises();
     }
     catch (ex) {
       vmState.hasError = true;
       viewContract.showError(ex.toString());
+    }
+    finally {
+      stopLoadingState();
+    }
+  }
+
+  Future _loadWorkoutExercises() async {
+    vmState.workoutExercises.clear();
+
+    if (!vmState.isAdd) {
+      vmState.workoutExercises.addAll(await _workoutExerciseInteractor.get(criteria: ('workout_id', vmState.workout.id)));
+      vmState.workoutExercises.sort((a, b) => a.sequence.compareTo(b.sequence));
     }
   }
 
   Future _loadCategories() async {
-    vmState.hasError = false;
-
-    try {
-      vmState.categories.clear();
-      vmState.categories.addAll(await _workoutInteractor.getCategories());
-    }
-    catch (ex) {
-      vmState.hasError = true;
-      viewContract.showError(ex.toString());
-    }
+    vmState.categories.clear();
+    vmState.categories.addAll(await _workoutInteractor.getCategories());
   }
 
   Future _loadExercises() async {
-    vmState.hasError = false;
-
-    try {
-      vmState.exercises.clear();
-      vmState.exercises.addAll(await _exerciseInteractor.get());
-    }
-    catch (ex) {
-      vmState.hasError = true;
-      viewContract.showError(ex.toString());
-    }
+    vmState.exercises.clear();
+    vmState.exercises.addAll(await _exerciseInteractor.get());
   }
 
   @override
@@ -83,6 +64,7 @@ class WorkoutEditViewModel extends BaseViewModel<WorkoutEditViewModelState, Work
     try {
       if (!vmState.isAdd) {
         _workoutInteractor.remove(vmState.workout);
+        notifyListeners();
       }
     }
     catch (ex) {
@@ -97,6 +79,7 @@ class WorkoutEditViewModel extends BaseViewModel<WorkoutEditViewModelState, Work
 
     try {
       _workoutInteractor.save(vmState.workout);
+      notifyListeners();
     }
     catch (ex) {
       vmState.hasError = true;
@@ -110,6 +93,7 @@ class WorkoutEditViewModel extends BaseViewModel<WorkoutEditViewModelState, Work
 
     try {
       _workoutExerciseInteractor.save(workoutExercise);
+      notifyListeners();
     }
     catch (ex) {
       vmState.hasError = true;
