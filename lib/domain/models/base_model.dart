49504361f1654920510fc985cc7b4c4e0014e2f3
Keith McAlpine <mcalpinekeith@yahoo.com>
2023-12-14 09:17:34 -0600

    BaseMapModel

--- lib/domain/models/base_model.dart
+++ lib/domain/models/base_model.dart
@@ -1,7 +1,10 @@
-abstract class BaseModel {
-  String get path;
+abstract class BaseModel extends BaseMapModel {
   late String id;
+  late DateTime createdAt;
+  String get path;
+}
 
+abstract class BaseMapModel {
   dynamic fromMap(Map<String, dynamic> map);
   Map<String, dynamic> toMap();
 }
