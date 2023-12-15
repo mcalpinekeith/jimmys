883baa7689a49966431b96987fdc884e028fe7c9
Keith McAlpine <mcalpinekeith@yahoo.com>
2023-12-15 09:27:18 -0600

    workout

--- lib/domain/models/workout_exercise.dart
+++ lib/domain/models/workout_exercise.dart
@@ -1,58 +1,64 @@
-import 'package:jimmys/core/extensions/iterable.dart';
 import 'package:jimmys/core/extensions/map.dart';
 import 'package:jimmys/domain/models/base_model.dart';
 import 'package:jimmys/domain/models/exercise.dart';
+import 'package:jimmys/domain/models/workout.dart';
+import 'package:uuid/uuid.dart';
 
-class WorkoutExercise implements BaseModel {
+class WorkoutExercise<T extends BaseMapModel> implements BaseModel {
   WorkoutExercise.empty();
+
+  WorkoutExercise.create() {
+    id = const Uuid().v8();
+  }
 
   WorkoutExercise({
     required this.id,
+    DateTime? createdAt,
     required this.workoutId,
     required this.exerciseId,
-    this.isDropSet = false,
     required this.sequence,
-    List<String>? sets,
+    required this.meta,
+    this.workout,
     this.exercise,
-    this.supersetExerciseId = '',
-    this.supersetExercise,
     this.isShared = false,
-  }) : sets = sets ?? [];
+  }) :
+    createdAt = createdAt ?? DateTime.now();
+
+  static const sequenceDefaultValue = 1;
 
   @override
   String id = '';
   @override
+  DateTime createdAt = DateTime.now();
+  @override
   String get path => 'workout_exercises';
+
   String workoutId = '';
   String exerciseId = '';
-  bool isDropSet = false;
-  int sequence = 0;
-  List<String> sets = [];
+  int sequence = sequenceDefaultValue;
+  late T? meta;
 
+  late Workout? workout;
   late Exercise? exercise;
-  String? supersetExerciseId;
-  late Exercise? supersetExercise;
   bool isShared = false;
 
   @override
   WorkoutExercise fromMap(Map<String, dynamic> map) => WorkoutExercise(
-    id: map.value('id', ''),
-    workoutId: map.value('workout_id', ''),
-    exerciseId: map.value('exercise_id', ''),
-    isDropSet: map.value('is_drop_set', false),
-    sequence: map.value('sequence', 1),
-    sets: (map.value('sets', <String>[]) as Iterable).toStringList(),
-    supersetExerciseId: map.value('superset_exercise_id', ''),
+    id: map.value('id'),
+    createdAt: map.value('created_at'),
+    workoutId: map.value('workout_id'),
+    exerciseId: map.value('exercise_id'),
+    sequence: map.value('sequence', defaultValue: sequenceDefaultValue),
+    meta: (T as BaseMapModel).fromMap(map.value('meta')),
   );
 
   @override
   Map<String, dynamic> toMap() => {
     'id': id,
+    'created_at': createdAt,
     'workout_id': workoutId,
     'exercise_id': exerciseId,
-    'is_drop_set': isDropSet,
     'sequence': sequence,
-    'sets': sets,
-    'superset_exercise_id': supersetExerciseId,
+    'meta': (T as BaseMapModel).toMap(),
   };
 }
